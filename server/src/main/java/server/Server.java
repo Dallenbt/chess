45d26a1705fcece19425e28857e55b2b98fc634f
Dallen <dallenafish11@gmail.com>
2025-10-14 10:36:58 -0600

    worked on getting a error when you are createing a already existing user

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,7 @@
 package server;
 
 import com.google.gson.Gson;
+import dataaccess.MemoryDataAccess;
 import datamodel.UserData;
 import io.javalin.*;
 import io.javalin.http.Context;
@@ -14,7 +15,8 @@ public class Server {
     private final UserService userService;
 
     public Server() {
-        userService = new UserService();
+        var dataAccess = new MemoryDataAccess();
+        userService = new UserService(dataAccess);
         server = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
@@ -25,13 +27,19 @@ public class Server {
     }
 
     private void register(Context ctx){
-        var serilaizer = new Gson();
-        String requestJson = ctx.body();
-        var user = serilaizer.fromJson(requestJson, UserData.class);
+        try {
+            var serilaizer = new Gson();
+            String requestJson = ctx.body();
+            var user = serilaizer.fromJson(requestJson, UserData.class);
 
-        var authData = userService.register(user);
+            var authData = userService.register(user);
 
-        ctx.result(serilaizer.toJson(authData));
+            ctx.result(serilaizer.toJson(authData));
+        }
+        catch (Exception ex){
+            var msg = String.format("{ \"message\": \"Error: already taken\" }", ex.getMessage());
+            ctx.status(403).result(msg);
+        }
     }
 
     public int run(int desiredPort) {
