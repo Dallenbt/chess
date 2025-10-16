7f27e83e5361023a400d49efa62c0a91f570b5bb
Dallen <dallenafish11@gmail.com>
2025-10-16 10:34:18 -0600

    wrote test for dataaccess

---
+++ server/src/test/java/dataaccess/DataAccessTest.java
@@ -0,0 +1,41 @@
+package dataaccess;
+
+import datamodel.UserData;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataAccessTest {
+
+    @Test
+    void clear() {
+        DataAccess db = new MemoryDataAccess();
+        db.createUser(new UserData("Joe", "j@j.com", "toomanysecrets"));
+        db.clear();
+        assertNull(db.getUser("Joe"));
+    }
+
+    @Test
+    void createUser() {
+        DataAccess db = new MemoryDataAccess();
+        var user = new UserData("Joe", "j@j.com", "toomanysecrets");
+        db.createUser(user);
+        assertEquals(user, db.getUser(user.username()));
+    }
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void createAuth() {
+    }
+
+    @Test
+    void getAuth() {
+    }
+
+    @Test
+    void deleteAuth() {
+    }
+}
