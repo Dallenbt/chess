c8cce4f5e6a8fe9d1b5650fc1203a2e92efc9918
Dallen <dallenafish11@gmail.com>
2025-10-18 14:01:43 -0600

    Added tests for listGames

--- server/src/test/java/service/GameServiceTest.java
+++ server/src/test/java/service/GameServiceTest.java
@@ -10,7 +10,27 @@ import static org.junit.jupiter.api.Assertions.*;
 class GameServiceTest {
 
     @Test
-    void listGames() {
+    void listGames() throws Exception {
+        DataAccess db = new MemoryDataAccess();
+        var user = new UserData("Joe", "j@j.com", "toomanysecrets");
+        var userService = new UserService(db);
+        var gameService = new GameService(db);
+        var authData = userService.register(user);
+        var game = gameService.createGame(authData.authToken(), "dubbin");
+        var listOfGames = gameService.listGames(authData.authToken());
+        assertNotNull(listOfGames);
+    }
+
+    @Test
+    void listGamesFail() throws Exception {
+        DataAccess db = new MemoryDataAccess();
+        var user = new UserData("Joe", "j@j.com", "toomanysecrets");
+        var userService = new UserService(db);
+        var gameService = new GameService(db);
+        var authData = userService.register(user);
+        var game = gameService.createGame(authData.authToken(), "dubbin");
+        assertThrows(Exception.class, () ->gameService.listGames(authData.username()));
+
     }
 
     @Test
@@ -57,6 +77,5 @@ class GameServiceTest {
         var authData = userService.register(user);
         var game = gameService.createGame(authData.authToken(), "dubbin");
         assertThrows(Exception.class, () -> gameService.joinGame(authData.authToken(), "grey", game.gameID()));
-
     }
 }
